466de28c2a35720a427d48fc963e2158b8431621
M.Kurowski <mikeleonkurowski1@gmail.com>
2026-06-25 15:24:52 +0200

    Figuren-Startaufstellung mittels Unicode-Zeichen hinzugefügt

--- src/main/java/schach/Schachbrett.java
+++ src/main/java/schach/Schachbrett.java
@@ -2,6 +2,7 @@ package schach;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -26,6 +27,42 @@ public class Schachbrett extends Application {
                 } else {
                     tile.setStyle("-fx-background-color: #708090");
                 }
+
+                Text Figur = null;
+
+                if (row == 0 && col == 0 || row == 0 && col == 7){
+                    Figur = new Text("♜");
+                }else if (row == 0 && col == 1 || row == 0 && col == 6){
+                    Figur = new Text("♞");
+                }else if (row == 0 && col == 2 || row == 0 && col == 5){
+                    Figur = new Text("♝");
+                }else if (row == 0 && col == 3){
+                    Figur = new Text("♛");
+                }else if (row == 0 && col == 4){
+                    Figur = new Text("♚");
+                }else if (row == 1) {
+                    Figur = new Text("♟");
+                }
+
+                else if (row == 7 && col == 0 || row == 7 && col == 7){
+                    Figur = new Text("♖");
+                } else if (row == 7 && col == 1 || row == 7 && col == 6){
+                    Figur = new Text("♘");
+                }else if (row == 7 && col == 2 || row == 7 && col == 5){
+                    Figur = new Text("♗");
+                } else if (row == 7 && col == 3){
+                    Figur = new Text("♕");
+                } else if (row == 7 && col == 4){
+                    Figur = new Text("♔");
+                }else if (row == 6){
+                    Figur = new Text("♙");
+                }
+                if (Figur != null){
+                    Figur.setStyle("-fx-font-size: 50px;");
+                    tile.getChildren().add(Figur);
+                }
+
+
                 tile.setPrefSize(Tile_size, Tile_size);
 
                 Board.add(tile, col, row);
